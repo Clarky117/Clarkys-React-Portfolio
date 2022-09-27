936e7e4f2e603e2f7dabbb76f744947aa2263556
Clarky <clarky117@outlook.com>
2022-09-28 00:46:13 +0800

    nav bar javascript functionality onClick successfully added, trialling of base layout and re routing to portfolio site to come

--- clarkys-react-portfolio/src/components/header/Header.jsx
+++ clarkys-react-portfolio/src/components/header/Header.jsx
@@ -4,40 +4,28 @@ import clarkyLogo from './clarky-logo.png'
 
 export default function Header() {
 
-  // js here?
-  // const navToggle = document.querySelector('.nav-toggle');
-  // const navLinks = document.querySelectorAll('.nav__link');
+  const navToggleAfterClick = () => {
+    document.body.classList.toggle('nav-open');
+  }
 
-  // navToggle.addEventListener('click', () => {
-  //   document.body.classList.toggle('nav-open');
-  // });
-
-  // navLinks.forEach(link => {
-  //   link.addEventListener('click', () => {
-  //     document.body.classList.remove('nav-open');
-  //   });
-  // });
-
-  // refactor above code into react syntax
-
-  // const navToggleAfterClick() => {
-
-  // }
+  const navLinksAfterClick = () => {
+    document.body.classList.remove('nav-open');
+  }
 
   return (
     <header>
       <div className="logo">
         <img src={clarkyLogo} alt="logo pic" />
       </div>
-      <button className="nav-toggle" aria-label="toggle navigation">
+      <button onClick={navToggleAfterClick} className="nav-toggle" aria-label="toggle navigation">
         <span className="hamburger"></span>
       </button>
       <nav className="nav">
         <ul className="nav__list">
-          <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
-          <li className="nav__item"><a href="#services" className="nav__link">My Services</a></li>
-          <li className="nav__item"><a href="#about" className="nav__link">About Me</a></li>
-          <li className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
+          <li onClick={navLinksAfterClick} className="nav__item"><a href="#home" className="nav__link">Home</a></li>
+          <li onClick={navLinksAfterClick} className="nav__item"><a href="#services" className="nav__link">My Services</a></li>
+          <li onClick={navLinksAfterClick} className="nav__item"><a href="#about" className="nav__link">About Me</a></li>
+          <li onClick={navLinksAfterClick} className="nav__item"><a href="#work" className="nav__link">My Work</a></li>
         </ul>
       </nav>
     </header>
