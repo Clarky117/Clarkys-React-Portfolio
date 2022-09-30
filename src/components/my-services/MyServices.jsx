d7ec3b9a856d50133d10bdf8339eb86d16dfdaf0
Clarky <clarky117@outlook.com>
2022-09-30 20:36:35 +0800

    can now download resume as pdf

--- src/components/my-services/MyServices.jsx
+++ src/components/my-services/MyServices.jsx
@@ -3,42 +3,47 @@ import { Link } from 'react-router-dom'
 import './MyServices.css'
 
 export default function MyServices() {
-  return (
-    // <!-- my services -->
-    <section className="my-services" id="services">
-        <h2 className="section__title section__title--services">What I do</h2>
-        <div className="services">
 
-            <div className="service">
-                <h3>JavaScript</h3>
-                <p>This is my preferred language along with its many libraries.
-                    Comfortable manipulating the DOM on the front end right through to 
-                    setting up full stack web applications.
-                </p>
+    return (
+        // <!-- my services -->
+        <section className="my-services" id="services">
+            <h2 className="section__title section__title--services">What I do</h2>
+            <div className="services">
+
+                <div className="service">
+                    <h3>React/JavaScript</h3>
+                    <p>This is my preferred language along with its many libraries.
+                        Comfortable manipulating the DOM on the front end right through to
+                        setting up full stack web applications.
+                    </p>
+                </div>
+
+                <div className="service">
+                    <h3>Node</h3>
+                    <p>Have acquired plenty of practise setting up full stack and back end applications
+                        by using Node and Express. In doing so have become familiar with reading
+                        new documentation and implementing new libraries that can be utilised through node.
+                    </p>
+                </div>
+
+                <div className="service">
+                    <h3>Databases</h3>
+                    <p>Enjoy playing with data in either a structured or non structured environmemt.
+                        Have used libraries such as MySQL2 and Mongoose to create models and perform
+                        CRUD operations on user data. Insomnia is my app of choice for testing end points.
+                    </p>
+                </div>
+
             </div>
+            {/* <!-- /services--> */}
 
-            <div className="service">
-                <h3>Node</h3>
-                <p>Have acquired plenty of practise setting up full stack and back end applications
-                     by using Node and Express. In doing so have become familiar with reading 
-                     new documentation and implementing new libraries that can be utilised through node. 
-                </p>
-            </div>
+            <a href="#work" className="btn">My Work</a>
+            <a href='clarkys-react-resume.pdf' className="btn" download>
+                Résumé
+            </a>
+            <Link to='/contact-me' className='btn'>Contact Me</Link>
 
-            <div className="service">
-                <h3>Databases</h3>
-                <p>Enjoy playing with data in either a structured or non structured environmemt. 
-                    Have used libraries such as MySQL2 and Mongoose to create models and perform 
-                    CRUD operations on user data. Insomnia is my app of choice for testing end points.
-                </p>
-            </div>
 
-        </div> 
-        {/* <!-- /services--> */}
-
-        <Link to='/contact-me' className='btn'>Contact Me</Link>
-        <a href="#work" className="btn">My Work</a>
-
-    </section>
-  )
+        </section>
+    )
 }
